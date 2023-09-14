c0bf9b6cab1cd169e1a5fcb34ea4ae3c37b35764
Ben Ehmke <benehmke@gmail.com>
2023-09-14 14:43:37 -0500

    DataTable docs

--- docs-src/helpers/source_helpers.rb
+++ docs-src/helpers/source_helpers.rb
@@ -20,8 +20,10 @@ module SourceHelpers
     content = body.match(/\/\*.*(?=\*\/)/m).to_s
     source = body.split(/\*\//, 2).last
 
+    file_name = file.gsub(/\.js$/, '')
     component_details = {
-      name: file.gsub(/\.js$/, '')
+      file_name: file_name,
+      name: file_name.gsub('-', '_').camelize
     }
     sections = content.split(/([^\n]+)\n(\-{2,})\n/)
     sections.each_with_index do |part, i|
@@ -31,8 +33,18 @@ module SourceHelpers
 
     match = source.match(/export\sdefault\sfunction\s#{component_details[:method]}\s?\((.*)\)/)
     component_details[:params] = match.try(:[], 1).try(:split, /\,\s?/)
+    
+    component_details[:name] = source.match(/export\s+default\s+class\s+(\w+)/).try(:[], 1)
+    component_details[:name] ||= file_name.gsub('-', '_').camelize
+    
+    if extended_class = source.match(/class\s+\w+\s+extends\s+(\w+)/).try(:[], 1)
+      if extended_class_path = source.match(/import[\s\{]+#{extended_class}[\s\}]+from\s+[\'\"]([\w\.\/]+)/).try(:[], 1)
+        component_details[:extends] = extended_class_path.match(/(\w+).js$/).try(:[], 1)
+      end
+    end
+    
     component_details[:source] = source.strip.sub('export default ', '')
-    component_details[:example_url] = "#{component_details[:name]}/example"
+    component_details[:example_url] = "#{component_details[:file_name]}/example"
     component_details
   end
   
--- docs-src/source/assets/javascripts/site.js
+++ docs-src/source/assets/javascripts/site.js
@@ -1,7 +1,10 @@
 // depends_on ../../../../lib/**/*.js
-import { Modal, Floater } from 'komps';
+import * as Komps from 'komps';
 import { addEventListenerFor } from 'dolla';
 
+Object.keys(Komps).forEach(funcName => {
+    window[funcName] = Komps[funcName]
+})
 
 document.addEventListener('DOMContentLoaded', function () {
     addEventListenerFor(document, '.js-toggle-source', 'click', e => {
