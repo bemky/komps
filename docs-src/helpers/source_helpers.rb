ddbda406584108d07baa09b65f286f5e72868224
Ben Ehmke <benehmke@gmail.com>
2023-11-22 12:34:50 -0600

    merge extended class attributes in docs

--- docs-src/helpers/source_helpers.rb
+++ docs-src/helpers/source_helpers.rb
@@ -25,11 +25,44 @@ module SourceHelpers
       file_name: file_name,
       name: file_name.gsub('-', '_').camelize
     }
+    
+    if extended_class = source.match(/class\s+\w+\s+extends\s+(\w+)/).try(:[], 1)
+      if extended_class_path = source.match(/import[\s\{]+#{extended_class}[\s\}]+from\s+[\'\"]([\w\.\/\-]+)/).try(:[], 1)
+        component_details[:extends] = extended_class_path.match(/(\w+).js$/).try(:[], 1)
+        extended_component = component(component_details[:extends])
+      end
+    end
+    
     sections = content.split(/([^\n]+)\n(\-{2,})\n/)
     
     sections.each_with_index do |part, i|
       next unless part =~ /^\-{2,}/
-      component_details[sections[i - 1]] = sections[i + 1]
+      key = sections[i - 1]
+      body = sections[i + 1]
+      
+      if body.exclude?("<")
+        code = []
+        body.gsub!(/```[^`]*```/) do |match|
+            code << match
+            "###code###"
+        end
+        yaml = ""
+        body.gsub!(/^\s*[\w\[\]\'\"]+:.*/) do |match|
+            yaml += match + "\n"
+            ""
+        end
+        body.gsub!("###code###") do |match|
+            code.shift
+        end
+      end
+      
+      attributes = yaml.present? ? YAML.load(yaml) : {}
+      attributes = extended_component[key][:attributes].merge(attributes) if extended_component.try(:[], key).try(:[], :attributes)
+      
+      component_details[key] = {
+        body: body,
+        attributes: attributes
+      }
     end
 
     match = source.match(/export\sdefault\sfunction\s#{component_details[:method]}\s?\((.*)\)/)
@@ -37,12 +70,6 @@ module SourceHelpers
     
     component_details[:name] = source.match(/export\s+default\s+class\s+(\w+)/).try(:[], 1)
     component_details[:name] ||= file_name.gsub('-', '_').camelize
-    
-    if extended_class = source.match(/class\s+\w+\s+extends\s+(\w+)/).try(:[], 1)
-      if extended_class_path = source.match(/import[\s\{]+#{extended_class}[\s\}]+from\s+[\'\"]([\w\.\/]+)/).try(:[], 1)
-        component_details[:extends] = extended_class_path.match(/(\w+).js$/).try(:[], 1)
-      end
-    end
     
     component_details[:source] = source.strip.sub('export default ', '')
     component_details[:example_url] = "#{component_details[:file_name]}/example"
