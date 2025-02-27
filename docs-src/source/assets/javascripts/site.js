7900a4c4ef54c6cf80787363093201152407f21f
Ben Ehmke <benehmke@gmail.com>
2025-02-27 17:04:21 -0600

    add collapsible plugin for Table

--- docs-src/source/assets/javascripts/site.js
+++ docs-src/source/assets/javascripts/site.js
@@ -1,13 +1,13 @@
 // depends_on ../../../../lib/**/*.js
 import * as Komps from 'komps';
-import { resizable, reorderable } from 'komps/plugins';
+import { resizable, reorderable, collapsible } from 'komps/plugins';
 import { addEventListenerFor } from 'dolla';
 
 Object.keys(Komps).forEach(funcName => {
     window[funcName] = Komps[funcName]
 })
 window.plugins = {
-    resizable, reorderable
+    resizable, reorderable, collapsible
 }
 
 document.addEventListener('DOMContentLoaded', function () {
